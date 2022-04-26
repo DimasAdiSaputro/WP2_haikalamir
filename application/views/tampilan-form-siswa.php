cf5cb69da38995e9570edcbc24c625c2a344c12b
amirhaikaal <haikalamir221@gmail.com>
2022-04-26 19:50:56 +0700

    Latihan

---
+++ application/views/tampilan-form-siswa.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Implementasi Model Prototipe Pada Digital Learning Management System (Di-Lemas)</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/tampilanform.css" type="text/css">
+    <title>Tampilan Form siswa</title>
+</head>
+<body>
+    <?php echo validation_errors(); ?>
+    <form action="<?= base_url('datasiswa/cetak'); ?>"method="post">
+    <th><h2>Implementasi Model Prototipe Pada Digital Learning Management System (Di-Lemas)</h2></th>
+    <table>
+        
+    </table>
+</form>
+</body>
+</html>
