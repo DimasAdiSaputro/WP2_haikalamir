bdade7a3ce25c711566bddb25e50b65a99c87828
amirhaikaal <haikalamir221@gmail.com>
2022-04-26 19:01:03 +0700

    pertemuan 6 dan 7

--- application/controllers/Autentikasi.php
+++ application/controllers/Autentikasi.php
@@ -5,7 +5,7 @@ class Autentikasi extends CI_Controller{
     public function index()
     {
         $this->load->view('templates/auto_header');
-        $this->load->view('autentikasi/login');
+        $this->load->view('admin/login');
         $this->load->view('templates/auto_footer');
 
     }
